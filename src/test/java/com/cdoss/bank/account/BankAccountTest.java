2c3bb09ebe9ec36ab6e50f1c3de932bacdc909f3
Chandy Doss <chandy.doss@gmail.com>
2018-03-04 12:14:27 -0500

    unit test for creating an account

---
+++ src/test/java/com/cdoss/bank/account/BankAccountTest.java
@@ -0,0 +1,24 @@
+package com.cdoss.bank.account;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+
+public class BankAccountTest {
+
+	private Account account;
+
+	@Test
+	public void createBankAccount() {
+		account = new CheckingAccount();
+		assertNotNull(account);
+	}
+
+	@Test
+	public void checkBalanaceOfNewAccount() {
+		account = new CheckingAccount();
+		assertEquals(0.00, account.getBalance(), 0.00);
+	}
+
+}

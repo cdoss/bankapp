6daa3e905203edba65b672dc8d7d723ab0b123a3
Chandy Doss <chandy.doss@gmail.com>
2018-03-04 09:57:54 -0500

    Initial project setup

---
+++ src/main/java/com/cdoss/bank/BankApp.java
@@ -0,0 +1,13 @@
+package com.cdoss.bank;
+
+/**
+ * Hello world!
+ *
+ */
+public class BankApp
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

4662e30b26319d7a50310b925db97287c15dc431
Chandy Doss <chandy.doss@gmail.com>
2018-10-22 23:22:49 -0400

    made bank app a Spring boot application

--- src/main/java/com/cdoss/bank/BankApp.java
+++ src/main/java/com/cdoss/bank/BankApp.java
@@ -1,13 +1,12 @@
 package com.cdoss.bank;
 
-/**
- * Hello world!
- *
- */
-public class BankApp
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankApp.class, args);
+	}
 }

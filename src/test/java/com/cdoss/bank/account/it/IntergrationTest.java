221b64f8e4db44d0510b4355c955ad6e23596954
Chandy Doss <chandy.doss@gmail.com>
2018-11-05 09:49:58 -0500

    using h2 for unit testing

--- src/test/java/com/cdoss/bank/account/it/IntergrationTest.java
+++ src/test/java/com/cdoss/bank/account/it/IntergrationTest.java
@@ -1,12 +1,27 @@
 package com.cdoss.bank.account.it;
 
-import org.junit.Test;
+import javax.annotation.Resource;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.cdoss.bank.entity.Customer;
+import com.cdoss.bank.repository.CustomerRepository;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class IntergrationTest {
 
+	@Resource
+	private CustomerRepository customerRepository;
+	
 	@Test
 	public void test() {
-		// fail("Not yet implemented");
+		Customer customer = customerRepository.findById(1);
+		Assert.assertEquals("John", customer.getFirstName());
 	}
 
 }

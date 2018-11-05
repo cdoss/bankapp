60a9617afa0eeed6770151ada3af7a2bd74cb053
Chandy Doss <chandy.doss@gmail.com>
2018-11-05 07:02:38 -0500

    integration tests have been marked as SpringBootTest

--- src/test/java/com/cdoss/bank/account/it/IntergrationTest.java
+++ src/test/java/com/cdoss/bank/account/it/IntergrationTest.java
@@ -1,12 +1,35 @@
 package com.cdoss.bank.account.it;
 
-import org.junit.Test;
+import java.util.List;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.cdoss.bank.entity.Customer;
+import com.cdoss.bank.repository.CustomerRepository;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class IntergrationTest {
+
+	@Autowired
+	CustomerRepository customerRepository;
 
 	@Test
 	public void test() {
-		// fail("Not yet implemented");
+		Customer customer = new Customer();
+		customer.setFirstName("Jane");
+		customer.setLastName("Doe");
+
+		customerRepository.save(customer);
+		List<Customer> list = customerRepository.findAll();
+		System.out.println("Customers: " + list.toString());
+		customer = customerRepository.findById(1);
+		Assert.assertEquals("Jane", customer.getFirstName());
 	}
 
 }

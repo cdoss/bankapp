76ba6243ad5290edc405f34c2a44d2080a451d35
Chandy Doss <chandy.doss@gmail.com>
2018-11-12 23:17:22 -0500

    adding setter/getters for entity

--- src/main/java/com/cdoss/bank/entity/Account.java
+++ src/main/java/com/cdoss/bank/entity/Account.java
@@ -1,9 +1,83 @@
 package com.cdoss.bank.entity;
 
+import java.util.Currency;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "account")
 public class Account {
 
 	public Account() {
-		// TODO Auto-generated constructor stub
+
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	@Column(name = "customer_id")
+	private int customerId;
+	@Column(name = "balance")
+	private Currency balance;
+	@Column(name = "account_type")
+	private String accountType;
+	@Column(name = "is_frozen")
+	private boolean isFrozen;
+	@Column(name = "is_overdrafted")
+	private boolean isOverdrafted;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public Currency getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Currency balance) {
+		this.balance = balance;
+	}
+
+	public String getAccountType() {
+		return accountType;
+	}
+
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+
+	public boolean isFrozen() {
+		return isFrozen;
+	}
+
+	public void setFrozen(boolean isFrozen) {
+		this.isFrozen = isFrozen;
+	}
+
+	public boolean isOverdrafted() {
+		return isOverdrafted;
+	}
+
+	public void setOverdrafted(boolean isOverdrafted) {
+		this.isOverdrafted = isOverdrafted;
 	}
 
 }
--- src/main/java/com/cdoss/bank/entity/ContactInfo.java
+++ src/main/java/com/cdoss/bank/entity/ContactInfo.java
@@ -16,7 +16,7 @@ public class ContactInfo {
 	}
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id")
 	private int id;
 	@Column(name = "customer_id")
@@ -41,4 +41,101 @@ public class ContactInfo {
 	private String faxNumber;
 	@Column(name = "email_address")
 	private String emailAddress;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getAddress2() {
+		return address2;
+	}
+
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public String getHomePhone() {
+		return homePhone;
+	}
+
+	public void setHomePhone(String homePhone) {
+		this.homePhone = homePhone;
+	}
+
+	public String getBusinessPhone() {
+		return businessPhone;
+	}
+
+	public void setBusinessPhone(String businessPhone) {
+		this.businessPhone = businessPhone;
+	}
+
+	public String getCellPhone() {
+		return cellPhone;
+	}
+
+	public void setCellPhone(String cellPhone) {
+		this.cellPhone = cellPhone;
+	}
+
+	public String getFaxNumber() {
+		return faxNumber;
+	}
+
+	public void setFaxNumber(String faxNumber) {
+		this.faxNumber = faxNumber;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
 }

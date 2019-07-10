21938bf80137162c330d4ad664e6a631f27e59bc
Chandy Doss <chandy.doss@gmail.com>
2019-07-09 23:38:26 -0400

    annotating as repository

--- src/main/java/com/cdoss/bank/repository/ContactInfoRepository.java
+++ src/main/java/com/cdoss/bank/repository/ContactInfoRepository.java
@@ -1,9 +1,11 @@
 package com.cdoss.bank.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.cdoss.bank.entity.ContactInfo;
 
+@Repository
 public interface ContactInfoRepository extends CrudRepository<ContactInfo, Integer> {
 
 }

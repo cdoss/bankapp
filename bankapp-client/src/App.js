b0f12a4adcd5b3b37563bb23ca34db30cc85ea35
Chandy Doss <chandy.doss@gmail.com>
2019-09-01 11:18:13 -0400

    basic hello world client in reactjs

---
+++ bankapp-client/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Hello from './Hello'
+
+function App() {
+  return (
+    <Hello object="World"/>
+  );
+}
+
+export default App;
---
+++ bankapp-client/src/Hello.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Hello({object}) {
+    return (
+        <div>
+            Hello, <span>{object}</span>!
+        </div>
+    )
+}

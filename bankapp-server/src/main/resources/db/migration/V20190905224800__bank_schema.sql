83ff33a69248a6e1fa699dc399f19698df7bafc2
Chandy Doss <chandy.doss@gmail.com>
2019-09-07 09:15:40 -0400

    fixing foreign key constraint issue

--- bankapp-server/src/main/resources/db/migration/V20190905224800__bank_schema.sql
+++ bankapp-server/src/main/resources/db/migration/V20190905224800__bank_schema.sql
@@ -1,15 +1,16 @@
 DROP TABLE IF EXISTS customer;
 CREATE TABLE customer(
-	id INT AUTO_INCREMENT PRIMARY KEY,
+	id INT AUTO_INCREMENT,
 	first_name VARCHAR(64),
 	middle_initial VARCHAR(1),
 	last_name VARCHAR(64),
-	suffix VARCHAR(32)
+	suffix VARCHAR(32),
+	PRIMARY KEY(id)
 );
 
 DROP TABLE IF EXISTS contact_info;
 CREATE TABLE contact_info(
-	id INT AUTO_INCREMENT PRIMARY KEY,
+	id INT AUTO_INCREMENT,
 	customer_id INT,
 	address VARCHAR(64),
 	address2 VARCHAR(64),
@@ -21,23 +22,27 @@ CREATE TABLE contact_info(
 	cell_phone VARCHAR(32),
 	fax_number VARCHAR(32),
 	email_address VARCHAR(64),
-	FOREIGN KEY (customer_id) REFERENCES customer(id)
+	PRIMARY KEY(id),
+	FOREIGN KEY fk_customer(customer_id) REFERENCES customer(id) ON DELETE CASCADE
 );
 
 DROP TABLE IF EXISTS account_type;
 CREATE TABLE account_type(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(32)	
+	id INT AUTO_INCREMENT,
+	name VARCHAR(32),
+	PRIMARY KEY(id),
+	UNIQUE KEY(name)
 );
 
 DROP TABLE IF EXISTS account;
 CREATE TABLE account(
-	id BIGINT AUTO_INCREMENT PRIMARY KEY,
+	id BIGINT AUTO_INCREMENT,
 	customer_id INT,
 	balance BIGINT,
 	account_type VARCHAR(32),
 	is_frozen BOOLEAN,
 	is_overdrafted BOOLEAN,
-	FOREIGN KEY (customer_id) REFERENCES customer(id),
-	FOREIGN KEY (account_type) REFERENCES account_type(name)
+	PRIMARY KEY(id),
+	FOREIGN KEY fk_customer(customer_id) REFERENCES customer(id) ON DELETE CASCADE,
+	FOREIGN KEY fk_account_type(account_type) REFERENCES account_type(name)
 );

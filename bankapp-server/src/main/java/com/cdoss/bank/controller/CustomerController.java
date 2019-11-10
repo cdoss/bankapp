87cd7a269e71bd89b69129d8dd8d0fa5f6fb77f3
Chandy Doss <chandy.doss@gmail.com>
2019-11-10 13:03:34 -0500

    moving findAllCustomers into service class

--- bankapp-server/src/main/java/com/cdoss/bank/controller/CustomerController.java
+++ bankapp-server/src/main/java/com/cdoss/bank/controller/CustomerController.java
@@ -1,6 +1,5 @@
 package com.cdoss.bank.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cdoss.bank.entity.Customer;
-import com.cdoss.bank.repository.CustomerRepository;
+import com.cdoss.bank.service.CustomerService;
 
 @RestController
 public class CustomerController {
@@ -18,17 +17,11 @@ public class CustomerController {
 	}
 
 	@Autowired
-	private CustomerRepository repository;
+	private CustomerService customerService;
 
 	@GetMapping(value = "/customers")
 	public List<Customer> findAllCustomers() {
-		List<Customer> customers = new ArrayList<>();
-
-		Iterable<Customer> results = this.repository.findAll();
-		results.forEach(customer -> {
-			customers.add(customer);
-		});
-
-		return customers;
+		return customerService.findAllCustomers();
+	}
 	}
 }
--- bankapp-server/src/main/java/com/cdoss/bank/service/CustomerService.java
+++ bankapp-server/src/main/java/com/cdoss/bank/service/CustomerService.java
@@ -1,8 +1,12 @@
 package com.cdoss.bank.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cdoss.bank.entity.Customer;
 import com.cdoss.bank.repository.AccountRepository;
 import com.cdoss.bank.repository.AccountTypeRepository;
 import com.cdoss.bank.repository.ContactInfoRepository;
@@ -25,5 +29,15 @@ public class CustomerService {
 	private final AccountTypeRepository accountTypeRepository;
 	private final ContactInfoRepository contactInfoRepository;
 	private final CustomerRepository customerRepository;
+	
+	public List<Customer> findAllCustomers() {
+		List<Customer> customers = new ArrayList<Customer>();
 
+		Iterable<Customer> results = this.customerRepository.findAll();
+		results.forEach(customer -> {
+			customers.add(customer);
+		});
+
+		return customers;
+	}
 }

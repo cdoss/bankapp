c814705df31fa7c515fafddeeb4b8dd80a33ceda
Chandy Doss <chandy.doss@gmail.com>
2019-11-17 12:37:40 -0500

    forgot to import Dashboard

--- bankapp-client/src/BankApp.js
+++ bankapp-client/src/BankApp.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
+import Dashboard from "./components/Dashboard";
 import Footer from "./components/Footer";
 
 export default function BankApp() {
